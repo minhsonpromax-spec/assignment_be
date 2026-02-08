42a969d56fb02bdcdc2f768ce1d0d11785d61bfa
minhsonpromax-spec <minhsonpromax@gmail.com>
2026-02-08 14:52:32 +0700

    Initial commit

---
+++ src/database/index.js
@@ -0,0 +1,11 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient({
+  log: ["query", "info", "warn", "error"],
+});
+
+process.on("beforeExit", async () => {
+  await prisma.$disconnect();
+});
+
+export default prisma;
